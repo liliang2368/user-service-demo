f617141f2a98b8e3d572f6befd7dfa6e22035483
liyang <2363582258@qq.com>
2020-08-28 17:41:57 +0800

    增加邮箱发送和验证码的书写

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+import com.example.demo.util.DbMailHelper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class DemoApplicationTests {
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes= DemoApplication.class) //这里DemoApplication要改成你的项目启动类名
+public class DemoApplicationTests {
+	@Autowired
+	private DbMailHelper dbMailHelper;
 	@Test
-	void contextLoads() {
+	public void Test1(){
+		/*填你的测试信息*/
+		String to ="1003177738@qq.com";
+		String title="密码找回";
+		String context ="本次验证哟9年公寓密码找回 请不要告知他人 验证码为"+dbMailHelper.verifyCode();
+		dbMailHelper.sendVertifyCode(to,title,context);
 	}
 
 }
