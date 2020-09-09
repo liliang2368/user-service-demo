822a7e04ea7fb10022dac36f8311b1c5f39b3a19
a1234 <2363582258@qq.com>
2020-09-09 18:10:31 +0800

    商品查询修改提交

---
+++ src/main/java/com/example/demo/dao/Interface/Produce1Mapper.java
@@ -0,0 +1,11 @@
+package com.example.demo.dao.Interface;
+
+import com.example.demo.dao.bo.Produce;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface Produce1Mapper {
+    List<Produce> FindProduct(String sku_name,Integer startindex,Integer pageSize);
+}
